69bdb77dfa983d407e662b15823e54623ce03fda
Daniel Waltz <danielbwaltz@gmail.com>
2026-05-01 16:59:22 -0400

    feat: add data fetching with pinia colada

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
+import { PiniaColada } from "@pinia/colada";
+import { createPinia } from "pinia";
 import { createVaporApp } from "vue";
 import { App } from "./App.tsrx";
 import "@/main.css";
 
-createVaporApp(App).mount("#app");
+const app = createVaporApp(App);
+
+app.use(createPinia());
+app.use(PiniaColada);
+
+app.mount("#app");
