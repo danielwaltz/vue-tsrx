a17229010c26f4fb5b4e1fabd4c84d4435ed5902
Daniel Waltz <danielbwaltz@gmail.com>
2026-05-02 11:04:46 -0400

    chore: configure prettier tailwind stylesheet

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "prettier";
+import type { PluginOptions } from "prettier-plugin-tailwindcss";
 
 export default {
   plugins: ["@tsrx/prettier-plugin", "prettier-plugin-tailwindcss"],
-} satisfies Config;
+  tailwindStylesheet: "./src/main.css",
+} satisfies Config & PluginOptions;
